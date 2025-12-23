ced49846763bd1263e117c6f7f95df9c9e5c128a
Harisankar <harisankar@uniqnex360.com>
2025-12-23 13:40:36 +0530

    chore(config): update CORS origins for Vercel

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Digital-Assets-Management"
     API_V1_STR: str = "/api/v1"
-    BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:5173"]
+    BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:5173","https://dam-lyart-iota.vercel.app"]
     STORAGE_PROVIDER: str = "local"
     POSTGRES_SERVER: str = "localhost"
     POSTGRES_USER: str = "postgres"
