0b464b897747f99f2360afe49332edb9df640962
Harisankar <harisankar@uniqnex360.com>
2026-06-11 17:14:47 +0530

    fix:user issue resolved

--- app/models/auth.py
+++ app/models/auth.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Boolean, text
+from sqlalchemy import Column, String, Boolean, DateTime, text
 from sqlalchemy.dialects.postgresql import UUID
 from .base import Base
 
@@ -12,6 +12,8 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     full_name = Column(String, nullable=True)
     is_active = Column(Boolean, default=True)
-
-    # role can be: 'admin' or 'user'
     role = Column(String, nullable=False, default='user', index=True)
+
+    created_at = Column(DateTime(timezone=True), server_default=text("now()"))
+    updated_at = Column(DateTime(timezone=True), server_default=text(
+        "now()"), onupdate=text("now()"))
