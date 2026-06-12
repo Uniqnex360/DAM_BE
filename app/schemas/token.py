bcf7e7058f19d312e4af50c948e10092bcea95dc
Harisankar <harisankar@uniqnex360.com>
2026-06-12 19:37:06 +0530

    fix:Token extended for impersonated_user

--- app/schemas/token.py
+++ app/schemas/token.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional,Any
+
 
 class Token(BaseModel):
     access_token: str
     token_type: str
-
+    impersonated_user: Optional[Any] = None
 class TokenPayload(BaseModel):
     sub: Optional[str] = None
